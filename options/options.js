76910917b6f14ae3a568a1d81a0a7339ef9000eb
Crowdbotics <team@crowdbotics.com>
2022-11-23 20:29:16 +0000

    Update options

--- options/options.js
+++ options/options.js
@@ -1,12 +1,23 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from "react-native"
+const duration = null
+const onDurationEnd = null
+const hide = () => SplashScreen.hide()
+const show = () => SplashScreen.show()
 
 export const globalOptions = {
   name: "live_voting_app_37629",
   url: "https://live_voting_app_37629.botics.co",
   api: "https://live_voting_app_37629.botics.co/api/v1"
 }
+
 export const modulesOptions = {
   "@modules/app-menu": {
-    "copy": "Routes available!"
+    copy: "Routes available!"
+  },
+  "@modules/splash": {
+    duration: duration,
+    onDurationEnd: onDurationEnd,
+    hide: hide,
+    show: show
   }
 }
